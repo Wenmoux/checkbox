e813615bf3b4bc07c85403583209dc055d071b04
Wenmoux <867970962a@gmail.com>
2020-12-29 11:39:50 +0800

    新增 龙空(lkong.cn)每日签到

---
+++ scripts/lkong.js
@@ -0,0 +1,25 @@
+const axios = require("axios")
+function task() {
+    return new Promise(async resolve => {
+        try {
+            let cookie=require("./config.json").lkong.cookie
+            let url = 'http://lkong.cn/index.php?mod=ajax&action=punch'            
+            let res = await axios.get(url,{headers:{"cookie":cookie}} )       
+             if(res.data.punchday)
+         {
+           msg=`签到成功✅已连签${res.data.punchday}天`         
+         }
+         else{
+           msg=res.data.error
+         }
+         console.log(msg)
+        } catch (err) {
+          console.log(err)         
+        }
+        resolve()
+    })
+}
+
+
+//task()
+module.exports=task
