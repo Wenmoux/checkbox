abd7ee2bb1b2b65a67679c7c48f2c51cb83ad0c3
ytgo <39256100+ytgo@users.noreply.github.com>
2021-10-01 12:25:55 +0800

    云原神签到

--- scripts/yunys.js
+++ scripts/yunys.js
@@ -8,8 +8,20 @@ function yunys() {
       const header = {
         headers: {
           Referer: "https://app.mihoyo.com/",
-          "x-rpc-combo_token": config.yunys.token,
-          "x-rpc-client_type": "2"
+          "x-rpc-combo_token": config.yunys.combo_token,
+          "x-rpc-client_type": config.yunys.type,
+		  "x-rpc-app_version": config.yunys.app_version,
+          "x-rpc-sys_version": config.yunys.sys_version,
+          "x-rpc-channel": "mihoyo",
+          "x-rpc-device_id": config.yunys.device_id,
+          "x-rpc-device_name": config.yunys.device_name,
+          "x-rpc-device_model": config.yunys.device_model,
+          "x-rpc-app_id": config.yunys.app_id,
+          "Referer": "https://app.mihoyo.com",
+          "Host": "api-cloudgame.mihoyo.com",
+          "Connection": "Keep-Alive",
+          "Accept-Encoding": "gzip",
+          "User-Agent": "okhttp/3.14.9"
         },
       };
       let res = await axios.get(url, header);
