ca622d176d3575931e2d19dcfb0b6e255e22ea12
wenmoux <wenmoux@gmail.com>
2022-04-03 21:58:06 +0800

    修改青龙配置文件路径

--- wencxinstall.sh
+++ wencxinstall.sh
@@ -1,13 +1,14 @@
 # cron: 0 0 * * *
 # new Env('签到盒安装&&依赖安装');
-if [ ! -f "/ql/config/config.yml" ]; then
- echo "生成配置文件 路径ql/config/config.yml"
- cp /ql/repo/Wenmoux_checkbox/config.yml.temple /ql/config/config.yml
+if [ ! -f "/ql/data/config/config.yml.yml" ]; then
+ echo "生成配置文件 路径ql/data/config/config.yml.yml"
+ cp /ql/repo/Wenmoux_checkbox/config.yml.temple /ql/data/config/config.yml.yml
 else
  echo "已存在配置文件"
- echo "生成空白模板 /ql/config/config.yml.sample"
- cp /ql/repo/Wenmoux_checkbox/config.yml.temple /ql/config/config.yml.sample
+ echo "生成空白模板 /ql/data/config/config.yml.yml.sample"
+ cp /ql/repo/Wenmoux_checkbox/config.yml.temple /ql/data/config/config.yml.yml.sample
 fi
+
 echo "cookie填写请去config/config.yml填写 青龙面板也有文件编辑功能"
 echo "安装依赖,不知道你的npm会不会报错我就npm pnpm一起用了 不会高大上的emmmm...."
 pnpm install  axios crypto crypto-js fs iconv-lite js-yaml yargs
