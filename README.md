47767a69f762e1a64eb99a97e0e1b62d27aba9b5
wenmoux <wenmoux@gmail.com>
2022-05-21 13:56:43 +0800

    docs: update readme.md

--- README.md
+++ README.md
@@ -114,7 +114,7 @@ termux可以使用crontab设置定时任务
 ## 青龙使用教程
   
 ```
-ql repo https://github.com/Wenmoux/checkbox.git "checkbox|install" "" "modules|scripts|config|Template|sendmsg"
+ql repo https://github.com/Wenmoux/checkbox.git "checkbox|install" "old" "modules|scripts|config|Template|sendmsg" "master"
 ```
  > 1 首先在青龙目录下config.sh设置里拉取sh后缀文件 大概这个自己找RepoFileExtensions  = xxx这一行 里边加上sh就可以了    
  2 在面板内添加定时任务 (上面ql repo那条 定时看你)    
--- wencxinstall_old.sh
+++ wencxinstall_old.sh
@@ -2,11 +2,11 @@
 # new Env('签到盒安装 旧青龙 &&依赖安装');
 if [ ! -f "/ql/config/config.yml" ]; then
  echo "生成配置文件 路径ql/config/config.yml"
- cp /ql/repo/Wenmoux_checkbox/config.yml.temple /ql/config/config.yml
+ cp /ql/repo/Wenmoux_checkbox_master/config.yml.temple /ql/config/config.yml
 else
  echo "已存在配置文件"
  echo "生成空白模板 /ql/config/config.yml.sample"
- cp /ql/repo/Wenmoux_checkbox/config.yml.temple /ql/config/config.yml.sample
+ cp /ql/repo/Wenmoux_checkbox_master/config.yml.temple /ql/config/config.yml.sample
 fi
 
 echo "cookie填写请去config/config.yml填写 青龙面板也有文件编辑功能"
