0bb39599d1e00c20757dedb366494aee93d27985
HaneRo <piumarugiada@hotmail.com>
2023-04-18 18:57:43 +0800

    刺猬猫添加自动投推荐票功能

--- scripts/hbooker.js
+++ scripts/hbooker.js
@@ -2,6 +2,8 @@ login_token = config.hbooker.token
 account = config.hbooker.account
 device_token = config.hbooker.device_token
 app_version = config.hbooker.app_version
+book_id = config.hbooker.book_id
+reader_id = config.hbooker.reader_id
 
 const CryptoJS = require("crypto-js");
 const axios = require("axios");
@@ -140,40 +142,39 @@ var sign = async function() {
     });
 };
 
-//test获取用户背包
-var give = async function() {
-    return await post({
-        url: "reader/get_my_info",
-        data: {
-
-        }
-    }).then(async (res) => {
-        console.log(res.data.prop_info.rest_recommend);
-        msg = "";
-        if (0!=res.data.prop_info.rest_recommend){//推荐票
-            return await post({
-                url: "book/give_recommend",
-                data: {
-                    book_id: book_id,
-                    count:res.data.prop_info.rest_recommend
-                }
-            }).then((res) => {
-                msg += "给"+book_id+"投出"+res.data.prop_info.rest_recommend+"张推荐票";
-            });
-        }
-        // if (0!=res.data.prop_info.rest_yp){//月票、未完
-        //     return await post({
-        //         url: "book/give_recommend",
-        //         data: {
-        //             book_id: book_id,
-        //             count:res.data.prop_info.rest_recommend
-        //         }
-        //     }).then((res) => {
-        //         return res;
-        //     });
-        // }
-        return msg;
-    });
+//获取用户背包
+var give = async function () {
+    if (book_id) {
+        return await post({
+            url: "reader/get_my_info",
+            data: {
+                reader_id: reader_id
+            }
+        }).then(async (res) => {
+            prop_info=res.data.prop_info
+            msg = "";
+            if (0 != prop_info.rest_recommend) {//推荐票
+                return await post({
+                    url: "book/give_recommend",
+                    data: {
+                        book_id: book_id,
+                        count: prop_info.rest_recommend
+                    }
+                }).then((res) => {
+                    if(res.code==100000){
+                        msg += "投出" + prop_info.rest_recommend + "张推荐票";
+                        console.log(msg);
+                        return msg;
+                    }else{
+                        console.log(prop_info);
+                        msg += "投推荐票失败";
+                        return msg;
+                    }
+                });
+            }
+            return msg;
+        });
+    }
 };
 
 //分享插画区帖子
@@ -322,8 +323,9 @@ async function hbooker() {
         result = a && a.tip
         console.log(a);
     }
-    console.log(result);
     b = await give();
+    result += b
+    console.log(result);
     return result;
 }
 
