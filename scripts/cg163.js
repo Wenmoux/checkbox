92670ef922a222749f7fcc4de8550bade89288ec
Night-stars-1 <nujj1042633805@gmail.com>
2024-08-08 23:52:34 +0800

    feat: 添加网易云游戏返回值解密，以获取更多信息 chore: 移除仓库下的node_modules, 并加入排除文件

--- scripts/cg163.js
+++ scripts/cg163.js
@@ -1,13 +1,25 @@
 const headers = {
   headers: {
-    Authorization:
-      config.cg163.Authorization || " bearer xxxxx",
+    Authorization: config.cg163.Authorization || " bearer xxxxx",
     "user-agent":
       "Mozilla/5.0 (Linux; Android 10; Redmi K30 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.127 Mobile Safari/537.36",
     //                "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
   },
+  validateStatus: status => status >= 200 && status < 300 || status === 400
 };
-const axios = require("axios")
+const axios = require("axios");
+function decrypt(data) {
+  const i = Buffer.from(data, "base64");
+
+  const result = i.map((item) => {
+    const n = (item - 301) % 256;
+    // 确保结果为正数
+    return n < 0 ? n + 256 : n;
+  });
+
+  return result.toString();
+}
+
 function check() {
   return new Promise(async (resolve) => {
     try {
@@ -16,7 +28,7 @@ function check() {
       console.log("cookie未失效,即将开始签到...");
       ckstatus = 1;
     } catch (err) {
-       console.log(err)
+      // console.log(err);
       console.log("cookie已失效");
       ckstatus = 0;
     }
@@ -28,15 +40,22 @@ function sign() {
   return new Promise(async (resolve) => {
     try {
       const url = `https://n.cg.163.com/api/v2/sign-today`;
-      let res = await axios.post(url, "", headers);
-      console.log("签到成功");
-      msg = "签到成功！！ ";
+      const res = await axios.post(url, "", headers);
+      const result = decrypt(res.data);
+      const data = JSON.parse(result);
+      if (data.hasOwnProperty("errmsgcn")) {
+        console.log("签到失败： " + data.errmsgcn);
+        msg = "签到失败： " + data.errmsgcn;
+      } else {
+        console.log("签到成功");
+        msg = "签到成功！！ ";
+      }
     } catch (err) {
-      //   console.log(err)
+      // console.log(err)
       msg = "签到失败,已签到过或其它未知原因！！ ";
       console.log(msg);
     }
-    resolve("【网易云游戏】："+msg);
+    resolve("【网易云游戏】：" + msg);
   });
 }
 async function cg163() {
