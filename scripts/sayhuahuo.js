e6ba8a4f8163c3004695ee0a6bcb63c3b5ed57d3
linsir <me@geel.cc>
2024-12-18 09:27:33 +0800

    Update sayhuahuo.js
    sayhuahuo域名更换

--- scripts/sayhuahuo.js
+++ scripts/sayhuahuo.js
@@ -1,6 +1,6 @@
 const rules = {
     name: "【花火论坛】： ",
-    url: "https://www.sayhuahuo.xyz/dsu_paulsign-sign.html", //用于获取formhash的链接
+    url: "https://www.sayhanabi.net/dsu_paulsign-sign.html", //用于获取formhash的链接
     cookie: config.huahuo.cookie,    
     formhash: 'name=\"formhash\" value=\"(.+?)\"', //formhash正则
     verify: "您需要先登录才能继续本操作", //验证cookie状态
@@ -8,7 +8,7 @@ const rules = {
         name: "签到",
         method: "post", 
         ua: "pc",
-        url: "https://www.sayhuahuo.xyz/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1", //签到链接
+        url: "https://www.sayhanabi.net/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1", //签到链接
         data: "formhash=@formhash&qdxq=kx&qmode=3&todaysay=&fastreply=0",
         reg2: "已经签到", //重复签到判断
         reg3: "签到成功", //签到成功判断
